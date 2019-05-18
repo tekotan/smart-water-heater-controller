af765878d7dda398aad42eea65b10b1426a01087
Tanish Baranwal <b.tanish@tugun.ai>
2019-05-18 10:05:57 -0700

    fixing scope

--- server/mechanical_ops.py
+++ server/mechanical_ops.py
@@ -5,7 +5,7 @@ my_servo = gz.Servo(26)
 my_servo.value = None
 
 
-def update(time):
+def update(seconds):
     my_servo.value = 0.1
-    time.sleep(time)
+    time.sleep(seconds)
     my_servo.value = None
